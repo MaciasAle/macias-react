8c241c2b1a691f6090711ed5d3b7038e259aae40
Alejandro <maciasale.ctf@gmail.com>
2024-11-04 18:18:42 -0300

    arreglando itemDetail

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -39,9 +39,35 @@
 
 // export default ItemDetailContainer;
 
-import React, { useContext, useEffect, useState } from "react";
+// import React, { useContext, useEffect, useState } from "react";
+// import { Image } from "@chakra-ui/react";
+// import { Stack, HStack, VStack } from "@chakra-ui/react";
+// import { Heading } from "@chakra-ui/react";
+// import { Text } from "@chakra-ui/react";
+
+// const ItemDetailContainer = ({ productById }) => {
+//   return (
+//     <div>
+//       <Heading size="md">{productById.name}</Heading>
+//       <Image src="" alt={`imagen de ${productById.name}`} borderRadius="lg" />
+//       <Stack mt="6" spacing="3">
+//         <Text color="blue.600" fontSize="2xl">
+//           Descripcion: {productById.description}
+//         </Text>
+//         <Text color="blue.600" fontSize="2xl">
+//           Precio: ${productById.price}
+//         </Text>
+//       </Stack>
+//     </div>
+//   );
+// };
+
+// export default ItemDetailContainer;
+
+// ItemDetailContainer.js
+import React from "react";
 import { Image } from "@chakra-ui/react";
-import { Stack, HStack, VStack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 
